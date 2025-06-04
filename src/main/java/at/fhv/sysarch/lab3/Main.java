34b1c90a83b09a5f17872f4847608003b58173c5
PascalPozzera <79970848+PascalPozzera@users.noreply.github.com>
2025-06-04 13:37:03 +0200

    created enum ViewConfig

--- src/main/java/at/fhv/sysarch/lab3/Main.java
+++ src/main/java/at/fhv/sysarch/lab3/Main.java
@@ -20,82 +20,62 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private final static int VIEW_WIDTH  = 860;
-    private final static int VIEW_HEIGHT = 540;
+    private static final int VIEW_WIDTH = 860;
+    private static final int VIEW_HEIGHT = 540;
+    private static final int SCENE_WIDTH = VIEW_WIDTH * 2;
+    private static final int SCENE_HEIGHT = VIEW_HEIGHT * 2;
+    private static final boolean USE_PUSH_PIPELINE = true;
 
-    private final static int SCENE_WIDTH  = VIEW_WIDTH * 2;
-    private final static int SCENE_HEIGHT = VIEW_HEIGHT * 2;
+    private enum ViewConfig {
+        ORANGE(Color.ORANGE, RenderingMode.POINT, false),
+        GREEN(Color.DARKGREEN, RenderingMode.WIREFRAME, false),
+        RED(Color.RED, RenderingMode.FILLED, false),
+        BLUE(Color.BLUE, RenderingMode.FILLED, true);
 
-    private final static boolean USE_PUSH_PIPELINE = true;
-    
+        final Color color;
+        final RenderingMode mode;
+        final boolean lighting;
+
+        ViewConfig(Color color, RenderingMode mode, boolean lighting) {
+            this.color = color;
+            this.mode = mode;
+            this.lighting = lighting;
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        File f = new File("resources/teapot.obj");
-        Optional<Model> om = ObjLoader.loadModel(f);
+        Optional<Model> optModel = ObjLoader.loadModel(new File("resources/teapot.obj"));
+        if (optModel.isEmpty()) return;
 
+        Model model = optModel.get();
         Group root = new Group();
-        Scene s = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT, Color.BLACK);
-        
-        final Canvas c1 = new Canvas(VIEW_WIDTH, VIEW_HEIGHT);
-        final Canvas c2 = new Canvas(VIEW_WIDTH, VIEW_HEIGHT);
-        final Canvas c3 = new Canvas(VIEW_WIDTH, VIEW_HEIGHT);
-        final Canvas c4 = new Canvas(VIEW_WIDTH, VIEW_HEIGHT);
-
         GridPane grid = new GridPane();
-        grid.add(c1, 0, 0);
-        grid.add(c2, 1, 0);
-        grid.add(c3, 0, 1);
-        grid.add(c4, 1, 1);
+
+        int i = 0;
+        for (ViewConfig config : ViewConfig.values()) {
+            int row = i / 2, col = i % 2;
+            Canvas canvas = new Canvas(VIEW_WIDTH, VIEW_HEIGHT);
+            grid.add(canvas, col, row);
+
+            PipelineData pd = new PipelineData.Builder(canvas, model, VIEW_WIDTH, VIEW_HEIGHT)
+                    .setModelColor(config.color)
+                    .setRenderingMode(config.mode)
+                    .setPerformLighting(config.lighting)
+                    .build();
+
+            AnimationTimer anim = USE_PUSH_PIPELINE
+                    ? PushPipelineFactory.createPipeline(pd)
+                    : PullPipelineFactory.createPipeline(pd);
+
+            anim.start();
+            i++;
+        }
 
         root.getChildren().add(grid);
-        stage.setScene(s);
+        stage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT, Color.BLACK));
         stage.setTitle("Simple CG Pipeline");
         stage.show();
-
-        om.ifPresent(m -> {
-            PipelineData pd1 = new PipelineData.Builder(c1, m, VIEW_WIDTH, VIEW_HEIGHT)
-                                    .setModelColor(Color.ORANGE)
-                                    .build();
-
-            PipelineData pd2 = new PipelineData.Builder(c2, m, VIEW_WIDTH, VIEW_HEIGHT)
-                                    .setModelColor(Color.DARKGREEN)
-                                    .setRenderingMode(RenderingMode.WIREFRAME)
-                                    .build();
-
-            PipelineData pd3 = new PipelineData.Builder(c3, m, VIEW_WIDTH, VIEW_HEIGHT)
-                                    .setModelColor(Color.RED)
-                                    .setRenderingMode(RenderingMode.FILLED)
-                                    .build();
-
-            PipelineData pd4 = new PipelineData.Builder(c4, m, VIEW_WIDTH, VIEW_HEIGHT)
-                                    .setModelColor(Color.BLUE)
-                                    .setRenderingMode(RenderingMode.FILLED)
-                                    .setPerformLighting(true)
-                                    .build();
-
-            AnimationTimer anim1;
-            AnimationTimer anim2;
-            AnimationTimer anim3;
-            AnimationTimer anim4;
-
-            if (USE_PUSH_PIPELINE) {
-                anim1 = PushPipelineFactory.createPipeline(pd1);
-                anim2 = PushPipelineFactory.createPipeline(pd2);
-                anim3 = PushPipelineFactory.createPipeline(pd3);
-                anim4 = PushPipelineFactory.createPipeline(pd4);
-
-            } else {
-                anim1 = PullPipelineFactory.createPipeline(pd1);
-                anim2 = PullPipelineFactory.createPipeline(pd2);
-                anim3 = PullPipelineFactory.createPipeline(pd3);
-                anim4 = PullPipelineFactory.createPipeline(pd4);
-            }
-
-            anim1.start();
-            anim2.start();
-            anim3.start();
-            anim4.start();
-        });
     }
 
     public static void main(String[] args) {
